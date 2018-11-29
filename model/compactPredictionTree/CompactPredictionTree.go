e8592592c68d94f22a25db687a29b4cbdd88e4a3
Madeddu <matteo.madeddu@adidas.com>
2018-11-29 20:44:15 +0100

    Change in intersection. WIP

--- model/compactPredictionTree/CompactPredictionTree.go
+++ model/compactPredictionTree/CompactPredictionTree.go
@@ -9,22 +9,29 @@ import (
 	"github.com/made2591/go-cpt/model/lookupTable"
 	"github.com/made2591/go-cpt/model/predictionTree"
 	"github.com/made2591/go-cpt/model/sequence"
+	set "github.com/deckarep/golang-set"
 )
 
 type CompactPredictionTree struct {
 	InvertedIndexTable *invertedIndexTable.InvertedIndexTable
 	LookupTable        *lookupTable.LookupTable
 	PredictionTree     *predictionTree.PredictionTree
+	TrainingSet        []*sequence.Sequence
+	TestingSet         []*sequence.Sequence
 }
 
 func NewCompactPredictionTree(
 	invertedIndexTable *invertedIndexTable.InvertedIndexTable,
 	lookupTable *lookupTable.LookupTable,
-	predictionTree *predictionTree.PredictionTree) (compactPredictionTree *CompactPredictionTree) {
+	predictionTree *predictionTree.PredictionTree,
+	trainingSet []*sequence.Sequence,
+	testingSet []*sequence.Sequence) (compactPredictionTree *CompactPredictionTree) {
 	compactPredictionTree = &CompactPredictionTree{}
 	compactPredictionTree.InvertedIndexTable = invertedIndexTable
 	compactPredictionTree.LookupTable = lookupTable
 	compactPredictionTree.PredictionTree = predictionTree
+	compactPredictionTree.TrainingSet = trainingSet
+	compactPredictionTree.TestingSet = testingSet
 
 	return compactPredictionTree
 }
@@ -51,78 +58,86 @@ func InitCompactPredictionTree(compactPredictionTree *CompactPredictionTree, seq
 
 }
 
-func PredictionOverTestingSequence(compactPredictionTree *CompactPredictionTree, targetSequence *sequence.Sequence, k int, n int) []string {
+func PredictionOverTestingSequence(compactPredictionTree *CompactPredictionTree, k int, n int) []string {
 
-	fmt.Println("original target: ", targetSequence.Values)
-	if k < len(targetSequence.Values) {
-		targetSequence = &sequence.Sequence{Values: targetSequence.Values[len(targetSequence.Values)-k:]}
+	intersection := set.NewSet()
+	for _, targetSequence := range compactPredictionTree.TestingSet {
+
 	}
-	fmt.Println("cut target: ", targetSequence.Values)
 
-	intersection := make([]int, 0)
-	for _, element := range targetSequence.Values {
-		for _, seq := range compactPredictionTree.InvertedIndexTable.Table[element] {
-			intersection = append(intersection, seq.ID)
+	for _, targetSequence := range compactPredictionTree.TestingSet {
+
+		fmt.Println("original target: ", targetSequence.Values)
+		if k < len(targetSequence.Values) {
+			targetSequence = &sequence.Sequence{Values: targetSequence.Values[len(targetSequence.Values)-k:]}
 		}
-	}
+		fmt.Println("cut target: ", targetSequence.Values)
 
-	similarSequences := make([]*sequence.Sequence, 0)
-	for element := range intersection {
-		fmt.Println(element)
-		currentNode := compactPredictionTree.LookupTable.Table[element]
-		tmp := &sequence.Sequence{Values: []string{}}
-		for currentNode != nil && currentNode.Parent != nil {
-			tmp.Values = append(tmp.Values, currentNode.Item)
-			currentNode = currentNode.Parent
-		}
-		for i := len(tmp.Values)/2 - 1; i >= 0; i-- {
-			opp := len(tmp.Values) - 1 - i
-			tmp.Values[i], tmp.Values[opp] = tmp.Values[opp], tmp.Values[i]
-		}
-		similarSequences = append(similarSequences, tmp)
-	}
-
-	// similarSequences := make([]*sequence.Sequence, 0)
-
-	fmt.Println("number of similar seqs: ", len(similarSequences))
-	for _, seq := range similarSequences {
-		fmt.Println("\t", seq.ID)
-	}
-
-	consequents := make(map[int]*sequence.Sequence, 0)
-	for _, similarSequence := range similarSequences {
-		consequent := sequence.ComputeConsequent(targetSequence, similarSequence)
-		if len(consequent.Values) > 0 {
-			consequents[similarSequence.ID] = sequence.ComputeConsequent(targetSequence, similarSequence)
-			fmt.Println("\t", similarSequence.ID, consequents[similarSequence.ID])
-		}
-	}
-
-	//fmt.Println("consequents: ")
-	countables := make(map[string]float64, 0)
-	for _, consequent := range consequents {
-		//fmt.Println(consequent)
-		for _, elem := range consequent.Values {
-			score := 0.0
-			if score, ok := countables[elem]; !ok {
-				score = float64(1+float64(1/len(similarSequences))+float64(1/(len(countables)+1))) * 0.001
-			} else {
-				score = score * float64(1+(1/len(similarSequences))+(1/(len(countables)+1))) * 0.001
+		for _, element := range targetSequence.Values {
+			for _, seq := range compactPredictionTree.InvertedIndexTable.Table[element] {
+				intersection.Add(seq.ID)
 			}
-			countables[elem] = score
-			//fmt.Println("\t", elem, score)
 		}
-	}
 
-	fmt.Println("number of counttable keys: ", len(countables))
-	pairs := rankByScore(countables)
-	result := make([]string, 0)
-	for _, pair := range pairs {
-		result = append(result, pair.Key)
-		fmt.Println("\t", pair.Key, pair.Value)
-	}
-	if n < len(result)-1 {
-		return result[:(n + 1)]
+		similarSequences := make([]*sequence.Sequence, 0)
+		it := set.Iterator()
+		for element := range it {
+			fmt.Println(element)
+			currentNode := compactPredictionTree.LookupTable.Table[element]
+			tmp := &sequence.Sequence{Values: []string{}}
+			for currentNode != nil && currentNode.Parent != nil {
+				tmp.Values = append(tmp.Values, currentNode.Item)
+				currentNode = currentNode.Parent
+			}
+			for i := len(tmp.Values)/2 - 1; i >= 0; i-- {
+				opp := len(tmp.Values) - 1 - i
+				tmp.Values[i], tmp.Values[opp] = tmp.Values[opp], tmp.Values[i]
+			}
+			similarSequences = append(similarSequences, tmp)
+		}
+
+		// similarSequences := make([]*sequence.Sequence, 0)
+
+		fmt.Println("number of similar seqs: ", len(similarSequences))
+		for _, seq := range similarSequences {
+			fmt.Println("\t", seq.ID)
+		}
+
+		consequents := make(map[int]*sequence.Sequence, 0)
+		for _, similarSequence := range similarSequences {
+			consequent := sequence.ComputeConsequent(targetSequence, similarSequence)
+			if len(consequent.Values) > 0 {
+				consequents[similarSequence.ID] = sequence.ComputeConsequent(targetSequence, similarSequence)
+				fmt.Println("\t", similarSequence.ID, consequents[similarSequence.ID])
+			}
+		}
+
+		//fmt.Println("consequents: ")
+		countables := make(map[string]float64, 0)
+		for _, consequent := range consequents {
+			//fmt.Println(consequent)
+			for _, elem := range consequent.Values {
+				score := 0.0
+				if score, ok := countables[elem]; !ok {
+					score = float64(1+float64(1/len(similarSequences))+float64(1/(len(countables)+1))) * 0.001
+				} else {
+					score = score * float64(1+(1/len(similarSequences))+(1/(len(countables)+1))) * 0.001
+				}
+				countables[elem] = score
+				//fmt.Println("\t", elem, score)
+			}
+		}
+
+		fmt.Println("number of counttable keys: ", len(countables))
+		pairs := rankByScore(countables)
+		result := make([]string, 0)
+		for _, pair := range pairs {
+			result = append(result, pair.Key)
+			fmt.Println("\t", pair.Key, pair.Value)
+		}
+		if n < len(result)-1 {
+			return result[:(n + 1)]
+		}
 	}
 	return result
 
